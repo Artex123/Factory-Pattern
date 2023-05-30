67d15cda5b362a49c4957f258ce9bcb7ee134944
Ben Zellers <zellersdenver@gmail.com>
2023-05-30 11:33:48 -0400

    Exercise 1 Factory of doom

--- FactoryPattern/Program.cs
+++ FactoryPattern/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace FactoryPattern
+﻿using System.Buffers.Text;
+using System.ComponentModel;
+
+namespace FactoryPattern
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var car = VehicleFactory.GetTire("four");
+
+            car.Drive();
+       
         }
     }
 }
---
+++ FactoryPattern/VehicleFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    public static class VehicleFactory
+    {
+        public static IVehicle GetTire(string wheels) 
+        { 
+            switch(wheels.ToLower()) 
+            {
+                case "four":
+                    return new Car();
+                case "two":
+                    return new Motorcycle();
+                default:
+                    return new Car();
+                     
+            }
+        }
+    }
+}
